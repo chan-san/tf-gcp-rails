0e778b69a67eb3f5ee74365cefbc7337a1985ace
chan-san <10119281+chan-san@users.noreply.github.com>
2022-02-28 04:04:25 +0900

    modify iam

--- terraform/modules/service_account/main.tf
+++ terraform/modules/service_account/main.tf
@@ -5,6 +5,24 @@ resource "google_service_account" "deployment_account" {
   display_name = "Deployment Service Account"
 }
 
+resource "google_project_iam_custom_role" "service_accounts_act_as" {
+  role_id     = "serviceAccountsActAs"
+  title       = "serviceAccountsActAs"
+  permissions = ["iam.serviceAccounts.actAs"]
+}
+
+resource "google_project_iam_member" "deployment_account_runAdmin" {
+  project = var.project
+  role    = "roles/run.admin"
+  member  = "serviceAccount:${google_service_account.deployment_account.email}"
+}
+
+resource "google_project_iam_member" "deployment_account_actAs" {
+  project = var.project
+  role    = google_project_iam_custom_role.service_accounts_act_as.id
+  member  = "serviceAccount:${google_service_account.deployment_account.email}"
+}
+
 resource "google_service_account" "app_account" {
   provider = google-beta
 
@@ -17,12 +35,6 @@ resource "google_project_iam_member" "app_account_secretAccessor" {
   role    = "roles/secretmanager.secretAccessor"
   member  = "serviceAccount:${google_service_account.app_account.email}"
 }
-// 複数付与する場合
-// resource "google_project_iam_member" "app_account_editor" {
-//   role    = "roles/editor"
-//   member  = "serviceAccount:${google_service_account.app_account.email}"
-// }
-
 
 # CloudRunを実行するサービスアカウント
 resource "google_service_account" "run_invoker_account" {
